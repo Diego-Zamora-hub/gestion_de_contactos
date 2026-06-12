8239531d731e194b34161b98d40bbeb734d1f600
Diego Zamora <zamoragallegodiegoalejandro@gmail.com>
2026-06-12 06:21:06 -0500

    Primera version

---
+++ bin/gestion_de_contactos.dart
@@ -0,0 +1,5 @@
+import 'package:gestion_de_contactos/gestion_de_contactos.dart' as gestion_de_contactos;
+
+void main(List<String> arguments) {
+  print('Hello world: ${gestion_de_contactos.calculate()}!');
+}
---
+++ test/gestion_de_contactos_test.dart
@@ -0,0 +1,8 @@
+import 'package:gestion_de_contactos/gestion_de_contactos.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
